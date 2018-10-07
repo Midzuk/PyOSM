3c47853ad333d075431ebdea7cbeea6d03041c22
Midzuki <ucdmzk@icloud.com>
2018-10-08 01:28:19 +0900

    修正

--- PyOSM+.py
+++ PyOSM+.py
@@ -5,7 +5,7 @@ def main():
 
   api = overpy.Overpass()
 
-  qry = 'node(50.745, 7.17, 50.75, 7.2);\
+  qry = 'node(35.635, 139.918, 35.647, 139.930);\
          way(bn)["highway"];\
          (._; >;);\
          out;'
@@ -14,7 +14,8 @@ def main():
   ways = result.ways
 
   with open('temporary/temp_nodes.csv', 'w') as node_file, open('temporary/temp_links.csv', 'w') as link_file:
-    node_file.write('node_id,lat,lon\n')
+    
+    node_file.write('node_id,lat,lon,traffic_signal\n')
     link_file.write('node_id_org,node_id_dest,distance,highway,oneway,bridge,tunnel\n')
 
     for way in ways:
@@ -27,10 +28,15 @@ def main():
       node_dict = {}
 
       for node in nodes:
-        node_dict[node.id] = (node.lat, node.lon)
+        node_highway = node.tags.get('highway')
+
+        if node_highway == 'traffic_signals':
+          node_dict[node.id] = (node.lat, node.lon, 'yes')
+        else:
+          node_dict[node.id] = (node.lat, node.lon, 'no')
       
-      for node_id, (lat, lon) in node_dict.items():
-        node_file.write('%d,%f,%f\n' % (node_id, lat, lon))
+      for node_id, (lat, lon, signal) in node_dict.items():
+        node_file.write('%d,%f,%f,%s\n' % (node_id, lat, lon, signal))
 
       for node1, node2 in zip(nodes, nodes[1:]):
         coord1 = (node1.lat, node1.lon)
@@ -45,70 +51,6 @@ if __name__ == '__main__':
   main()
   
 '''
-      coords_1 = (52.2296756, 21.0122287)
-      coords_2 = (52.406374, 16.9251681)
-      geopy.distance.vincenty(coords_1, coords_2).m
-    
-      vert1 = vertIter.next()
-      node1 = (vert1.x(), vert1.y(), layer)
-      
-      if node1 in nodes:
-          nodeId1 = nodes[node1]
-      else:
-          nodes[node1] = nodeIdCount
-          nodeId1 = nodeIdCount
-          nodeIdCount += 1
-          
-          nodeFile.write('%d,%f,%f,%i\n' % (nodeId1, vert1.x(), vert1.y(), layer))
-      
-      while vertIter.hasNext():
-          vert2 = vertIter.next()
-          node2 = (vert2.x(), vert2.y(), layer)
-          
-          if node2 in nodes:
-              nodeId2 = nodes[node2]
-          else:
-              nodes[node2] = nodeIdCount
-              nodeId2 = nodeIdCount
-              nodeIdCount += 1
-              
-              nodeFile.write('%d,%f,%f,%i\n' % (nodeId2, vert2.x(), vert2.y(), layer))
-          
-          #add links
-          link = (nodeId1, nodeId2)
-          
-          if link not in links:
-              pt1 = QgsPointXY(node1[0], node1[1])
-              pt2 = QgsPointXY(node2[0], node2[1])
-              
-              dist = da.measureLine([pt1, pt2])
-              
-              if feat['fclass']:
-                  fclass = feat['fclass']
-              else:
-                  fclass = ''
-              
-              if feat['oneway']:
-                  oneway = feat['oneway']
-              else:
-                  oneway = ''
-              
-              if feat['bridge']:
-                  bridge = feat['bridge']
-              else:
-                  bridge = ''
-                  
-              if feat['tunnel']:
-                  tunnel = feat['tunnel']
-              else:
-                  tunnel = ''
-              
-              linkFile.write('%d,%d,%f,%s,%s,%s,%s\n' % (nodeId1, nodeId2, dist, fclass, oneway, bridge, tunnel))
-              links.append(link)
-          
-          node1 = node2
-          nodeId1 = nodeId2
-
 qryNode = 'node(50.745, 7.17, 50.75, 7.2);\
             way(bn)["highway"];\
             node(w);\
